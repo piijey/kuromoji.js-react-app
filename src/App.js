9c940ffe2469c4e07abccb644393dfaa7c388bfa
piijey <pj.223f@gmail.com>
2024-04-16 20:55:14 +0900

    add dictionary selector

--- src/App.js
+++ src/App.js
@@ -3,17 +3,76 @@ import './App.css';
 var kuromoji = require("kuromoji");
 
 function App() {
+  const [ loading, setLoading ] = useState(true);
+  const [ message, setMessage ] = useState('辞書を選択してください');
+
+  const dictionaries = [
+    { dir: "kuromoji-dict", label: "IPA辞書" },
+    { dir: "kuromoji-dict-unidic", label: "UniDic" },
+    { dir: "kuromoji-dict-sudachi", label: "SudachiDict" },
+  ];
+  const [dictionaryDir, setDictionaryDir] = useState(null);
+
+  function DictionarySelector() {
+    const [selected, setSelected] = useState(dictionaryDir || dictionaries[0].dir);
+    function changeValue(e) { // ラジオボタンのクリックを画面に反映する
+      setSelected(e.target.value);
+    };
+
+    function handleSubmit() { // クリックされたら kuromoji.js に渡す辞書を変更し、ロード中にする
+      setDictionaryDir(selected);
+      setLoading(true);
+      setMessage(`トークナイザを読み込み中 ... ${selected}`)
+      console.log("選択された辞書:", selected);
+    }
+  
+    const dictOptions = dictionaries.map((dict, index) =>
+      <div className="form-check" key={index}>
+        <input
+         className="form-check-input"
+         type="radio"
+         id={`dict-${index}`}
+         name="dictionary"
+         value={dict.dir}
+         checked={dict.dir === selected}
+         onChange={changeValue}
+        />
+        <label className="form-check-label" htmlFor={`dict-${index}`}> {/* htmlFor属性を使ってラベルを関連付けることで、ラベルをクリックしてもチェックラジオに反映される */}
+          {dict.label}
+        </label>
+      </div>
+    );
+    return (
+      <div className='container dictionary-options'>
+        <div className="form-check d-flex row">
+          <div className='col-5'>
+            { dictOptions }
+          </div>
+          <div className='col-7'>
+            <button onClick={handleSubmit} className={`btn ${dictionaryDir ? 'btn-secondary' : 'btn-primary'}`}>
+              { dictionaryDir ? '変更' : '決定' }
+            </button>
+            <p>{ message }</p>
+          </div>
+          <div className='col'/>
+        </div>
+      </div>
+    );
+  }
+  
   const [userInputText, setUserInputText] = useState("");
   const [tokens, setTokens] = useState([]);
   const [tokenizer, setTokenizer] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => { //アプリのマウント時にkuromojiトークナイザを初期化
+  useEffect(() => { // handleSubmit によって辞書が選択されたら、kuromojiトークナイザを初期化
+    if ( !dictionaryDir ) {return}
+    setTokens([]);
+    setUserInputText('');
+
     function initializeTokenizer() {
       return new Promise((resolve, reject) => {
         kuromoji.builder(
-          //{ dicPath: process.env.PUBLIC_URL + "/kuromoji-dict/" } //IPADic
-          { dicPath: process.env.PUBLIC_URL + "/kuromoji-dict/" } //UniDic
+          { dicPath: process.env.PUBLIC_URL + dictionaryDir }
           ).build((err, buildTokenizer) => {
           if (err) {
               reject(err);
@@ -28,28 +87,33 @@ function App() {
       const startTime = performance.now();
       console.log("トークナイザをロードしています...");
       try {
-          const tokenizer = await initializeTokenizer();
-          setTokenizer(tokenizer);
-          console.log("トークナイザををロードしました");
+        const initializedTokenizer = await initializeTokenizer();
+        setTokenizer(initializedTokenizer);
+        setLoading(false);
+        const endTime = performance.now();
+        setMessage(`トークナイザのロードにかかった時間: ${(endTime - startTime).toFixed(2)} ms`);
+        console.log(`トークナイザのロードにかかった時間: ${(endTime - startTime).toFixed(2)} ms`);
       } catch (err) {
-          console.error("トークナイザのロードに失敗しました", err);
-      } finally {
-          setLoading(false);
-          const endTime = performance.now();
-          console.log(`トークナイザのロードにかかった時間: ${(endTime - startTime).toFixed(2)} ms`);
-      }
+        console.log(err)
+        if (err instanceof ProgressEvent) {
+          setMessage(`トークナイザが読み込めなかったよ、ぴぇっ。ほかの小さな辞書にしてみてね！ ${err}`);
+        } else if (err.message) {
+          setMessage(`トークナイザのロードに失敗しました: ${err.message || err}`);
+        };
+      };
     };
 
     loadTokenizer();
   // eslint-disable-next-line
-  }, []);
+  }, [dictionaryDir]);
 
   function analyze(event) {
     event.preventDefault();  // デフォルトのフォーム送信を阻止
     if (!tokenizer) {
-      console.error("トークナイザが利用できません");
+      setMessage("トークナイザが利用できません");
       return
     }
+    setMessage('');
 
     const formData = new FormData(event.target);
     const text = formData.get("text");
@@ -63,14 +127,21 @@ function App() {
   return (
     <div className='App'>
       <div className="card p-2 align-items-center">
-        {loading ? <>辞書を読み込み中 ... </> : <></> }
+        <h1> Try kuromoji.js </h1>
+        <DictionarySelector/>
+        { loading ? <>
+        {}
+        </> : <>
+        <div className='box-input-text'>
         <form onSubmit={analyze}>
           <div className='input-group'>
             <input name="text" type="text" className="form-control" placeholder="テキストを入力"/>
             <button type="submit" disabled={loading} className="btn btn-primary">解析</button>
           </div>
         </form>
-        <p>{userInputText}</p>
+        <div className='text-show'>{userInputText}</div>
+        </div>
+        <div className='box-show-result'>
         {/* トークンの情報を表形式で表示 */}
         <table>
           <thead>
@@ -94,6 +165,8 @@ function App() {
             ))}
           </tbody>
         </table>
+        </div>
+        </> }
       </div>
     </div>
   );
